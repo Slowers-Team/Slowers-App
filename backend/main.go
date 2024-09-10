b907faf5d73d9cab4d83a74c366038e49f840edc
Ville Sirviö <VSirvio@users.noreply.github.com>
2024-09-10 20:09:43 +0300

    Add BSON struct tags for Flower struct fields 'LatinName' and 'AddedTime' in backend/main.go

--- backend/main.go
+++ backend/main.go
@@ -16,8 +16,8 @@ import (
 type Flower struct {
 	ID         primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
 	Name       string             `json:"name"`
-	LatinName  string             `json:"latin_name"`
-	AddedTime  time.Time          `json:"added_time"`
+	LatinName  string             `json:"latin_name" bson:"latin_name"`
+	AddedTime  time.Time          `json:"added_time" bson:"added_time"`
 }
 
 var collection *mongo.Collection
