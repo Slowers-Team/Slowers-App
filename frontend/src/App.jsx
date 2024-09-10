5cc9588750e2b813793fe4ccc2da2d3b18b1cefc
Ville Sirviö <VSirvio@users.noreply.github.com>
2024-09-10 21:06:27 +0300

    Implement frontend for listing flowers

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,34 +1,43 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import axios from 'axios'
+import { useEffect, useState } from 'react'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const [flowers, setFlowers] = useState([])
+
+  useEffect(() => {
+    axios
+      .get('/api/flowers')
+      .then(response => setFlowers(response.data))
+  }, [])
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      {flowers && <FlowerList flowers={flowers} />}
     </>
+  )
+}
+
+const FlowerList = ({ flowers }) => {
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Latin name</th>
+          <th>Added time</th>
+        </tr>
+      </thead>
+      <tbody>
+        {flowers.map(flower => (
+          <tr key={flower.id}>
+            <td>{ flower.name }</td>
+            <td><em>{ flower.latin_name }</em></td>
+            <td>{ new Date(flower.added_time).toDateString() }</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
   )
 }
 
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,5 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import ReactDOM from 'react-dom/client'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import App from './App.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(<App />)
