3e8f11dd42fbf8a7fdad270cf738cf101695dbfe
Amanda <amanda.hamynen@gmail.com>
2024-09-10 14:54:15 +0300

    Add database connection code to backend

--- backend/main.go
+++ backend/main.go
@@ -1,9 +1,66 @@
 package main
 
 import (
-	"fmt"
+	"context"
+	"log"
+	"os"
+	"time"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/joho/godotenv"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
+type Flower struct {
+	ID         primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
+	Name       string             `json:"name"`
+	Latin_Name string             `json:"latin_name"`
+	Added_date time.Time          `json:"added_time"`
+}
+
+var collection *mongo.Collection
+
 func main() {
-	fmt.Println("Hello World")
+	if err := godotenv.Load(); err != nil {
+		log.Println("No .env file found")
+	}
+
+	mongoURI := os.Getenv("MONGODB_URI")
+	if mongoURI == "" {
+		log.Fatal("Set your 'MONGODB_URI' environment variable. " +
+			"See: " +
+			"www.mongodb.com/docs/drivers/go/current/usage-examples/#environment-variable")
+	}
+	clientOptions := options.Client().ApplyURI(mongoURI).SetTimeout(10 * time.Second)
+	client, err := mongo.Connect(context.Background(), clientOptions)
+	if err != nil {
+		panic(err)
+	}
+
+	defer func() {
+		if err := client.Disconnect(context.Background()); err != nil {
+			panic(err)
+		}
+	}()
+
+	if err := client.Ping(context.Background(), nil); err != nil {
+		panic(err)
+	}
+
+	log.Println("Connected to MongoDB")
+
+	collection = client.Database("Slowers").Collection("flowers")
+
+	app := fiber.New()
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "5001"
+	}
+
+	app.Static("/", "./client/dist")
+
+	log.Fatal(app.Listen("0.0.0.0:" + port))
 }
