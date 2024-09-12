af8845547759c9e910e5c2c682818766df42edb3
Halmela Mikko O <mikko.halmela@helsinki.fi>
2024-09-12 15:13:10 +0300

    containerize frontend

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,9 +7,10 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://127.0.0.1:5001',
+        target: 'http://backend:5001',
         changeOrigin: true,
       },
     },
+    host: true,
   },
 })
