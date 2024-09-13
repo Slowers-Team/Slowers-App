7f40825c50591ce332d4423c9366ec63d8f8da0a
Ville Sirviö <VSirvio@users.noreply.github.com>
2024-09-13 19:57:20 +0300

    Change the date formatting of the 'Added time' field in the frontend to a better one

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -97,16 +97,25 @@ const FlowerList = ({ flowers, handleDelete }) => {
         </tr>
       </thead>
       <tbody>
-        {flowers.map(flower => (
-          <tr key={flower.id}>
-            <td>{ flower.name }</td>
-            <td><em>{ flower.latin_name }</em></td>
-            <td>{ new Date(flower.added_time).toDateString() }</td>
-            <td>
-              <button onClick={() => handleDelete(flower)}>Delete</button>
-            </td>
-          </tr>
-        ))}
+        {flowers.map(flower => {
+          let addedTime = new Date(flower.added_time)
+
+          let date = addedTime.toLocaleDateString('fi')
+          let hour = addedTime.toLocaleString('fi', {hour: 'numeric'})
+          let minute = addedTime.toLocaleString('fi', {minute: '2-digit'})
+          let addedTimeStr = `${date} ${hour}:${minute}`
+
+          return (
+            <tr key={flower.id}>
+              <td>{ flower.name }</td>
+              <td><em>{ flower.latin_name }</em></td>
+              <td>{ addedTimeStr }</td>
+              <td>
+                <button onClick={() => handleDelete(flower)}>Delete</button>
+              </td>
+            </tr>
+          )
+        })}
       </tbody>
     </table>
   )
