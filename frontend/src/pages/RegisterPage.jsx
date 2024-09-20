22fb86e989141ba5ab9122cec228485e135fb975
Amanda <amanda.hamynen@gmail.com>
2024-09-20 15:22:08 +0300

    Remove confirmation when the user registers

--- frontend/src/pages/RegisterPage.jsx
+++ frontend/src/pages/RegisterPage.jsx
@@ -4,14 +4,12 @@ import RegisterForm from '../components/RegisterForm'
 const RegisterPage = () => {
 
     const createNewUser = userObject => {
-        if (window.confirm(`Are you sure you want to register a new user with email ${userObject.email}?`)) {
           userService
             .create(userObject)
             .catch(error => {
                 console.log(error)
                 alert('Registering failed')
             })  
-        }
     }
 
   return (
