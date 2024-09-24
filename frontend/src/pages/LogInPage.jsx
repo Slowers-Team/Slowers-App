1fccb864c6aca13fcdb41c0cf4d92855a702fd66
NUn <nun.lukkarinen01@gmail.com>
2024-09-24 14:19:57 +0300

    Added Login page formula

--- frontend/src/pages/LogInPage.jsx
+++ frontend/src/pages/LogInPage.jsx
@@ -1,35 +1,16 @@
-import React, { useState } from 'react'
-import { useNavigate } from 'react-router-dom'
-import LogIn from '../components/LogIn'
+import { useNavigate } from 'react-router-dom';
+import LogIn from '../components/LogIn';
 
-const LogInPage = () => {
+const LogInPage = ({ setIsLoggedIn }) => {
   const navigate = useNavigate();
 
-  const handleLogin = async (email, password) => {
-    try {
-      const response = await fetch('/api/loginReq', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({ email, password })
-      });
-
-      if (response.ok) {
-        const data = await response.json();
-        localStorage.setItem('token', data.token);
-        navigate('/');  // Oletus että kirjautuneille on oma näkymä
-      } else {
-        setError('Invalid email or password');
-      }
-    } catch (err) {
-      setError('An error occurred during login');
-    }
+  const handleLogin = () => {
+    navigate('/');  
   };
 
   return (
     <div>
-      <LogIn onLogin={handleLogin} />
+      <LogIn onLogin={handleLogin} setIsLoggedIn={setIsLoggedIn} />
     </div>
   );
 };
