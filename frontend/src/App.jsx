ffc1c1513b244b84c353b899a87810bf7643fd71
NUn <nun.lukkarinen01@gmail.com>
2024-09-24 14:17:59 +0300

    Added route login & logout

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,26 @@
-import './App.css'
-import RegisterPage from './pages/RegisterPage'
-import HomePage from './pages/HomePage'
-import LogInPage from './pages/LogInPage'
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
+import './App.css';
+import RegisterPage from './pages/RegisterPage';
+import HomePage from './pages/HomePage';
+import LogInPage from './pages/LogInPage';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { useState, useEffect } from 'react';
 
 const App = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);  
+
+  useEffect(() => {
+    const token = localStorage.getItem('token');
+    setIsLoggedIn(!!token);  
+  }, []);
+
+  const handleLogout = () => {
+    localStorage.removeItem('token');
+    setIsLoggedIn(false);
+  };
 
   const padding = {
-    padding: 5
-  }
+    padding: 5,
+  };
 
   return (
     <div>
@@ -16,18 +28,19 @@ const App = () => {
         <div>
           <nav>
             <Link style={padding} to="/">Home</Link>
-            <Link style={padding} to="/register">Register</Link>
-            <Link style={padding} to="/loginReq">Login</Link>
+            {!isLoggedIn && <Link style={padding} to="/register">Register</Link>}
+            {!isLoggedIn && <Link style={padding} to="/loginReq">Login</Link>}
+            {isLoggedIn && <Link onClick={handleLogout}>Logout</Link>}
           </nav>
           <Routes>
             <Route path="/" element={<HomePage />} />
-            <Route path="/loginReq" element={<LogInPage />} />
+            <Route path="/loginReq" element={<LogInPage setIsLoggedIn={setIsLoggedIn} />} />
             <Route path="/register" element={<RegisterPage />} />
           </Routes>
         </div>
       </Router>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
