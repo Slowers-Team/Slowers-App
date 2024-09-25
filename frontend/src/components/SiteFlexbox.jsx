77f9ed9c48802ca3e3a2b742efced4911025c48d
Sanni Isokirmo <sanni.isokirmo@helsinki.fi>
2024-09-25 13:32:11 +0300

    Fix tab spaces for flexboxes

--- frontend/src/components/SiteFlexbox.jsx
+++ frontend/src/components/SiteFlexbox.jsx
@@ -1,33 +1,33 @@
 import { useState } from 'react'
 
 const SiteFlexbox = () => {
-    const containerStyle = {
-        display: 'flex',
-        flexDirection: 'row',
-        flexWrap: 'wrap',
-        justifyContent: 'spacebetween',
-        width: '100%',
-        height: '100%',
-        border: '5px solid red'
-    }
+  const containerStyle = {
+    display: 'flex',
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    justifyContent: 'spacebetween',
+    width: '100%',
+    height: '100%',
+    border: '5px solid red'
+}
 
-    const boxStyle = {
-        width: '50px',
-        height: '100px',
-        colour: 'black',
-        padding: '10px',
-        border: '5px solid green'
-    }
+const boxStyle = {
+    width: '50px',
+    height: '100px',
+    colour: 'black',
+    padding: '10px',
+    border: '5px solid green'
+}
 
-    return (
-        <div style={containerStyle}>
-            <div style={boxStyle}>1</div>
-            <div style={boxStyle}>2</div>
-            <div style={boxStyle}>3</div>
-            <div style={boxStyle}>4</div>
-            <div style={boxStyle}>5</div>
-        </div>
-    )
+return (
+  <div style={containerStyle}>
+    <div style={boxStyle}>1</div>
+    <div style={boxStyle}>2</div>
+    <div style={boxStyle}>3</div>
+    <div style={boxStyle}>4</div>
+    <div style={boxStyle}>5</div>
+  </div>
+  )
 }
 
 export default SiteFlexbox
