8b2a62f7559c2f1b2bee22b61b56ae60bba4bf18
Halmela Mikko O <mikko.halmela@helsinki.fi>
2024-09-26 19:28:45 +0300

    document site service

--- frontend/src/services/sites.js
+++ frontend/src/services/sites.js
@@ -1,25 +1,97 @@
 import axios from 'axios'
 const baseUrl = '/api/sites'
 
+/*
+  Create a new site
+  input
+    newSite : {
+      name: string,
+      note: string,
+      parent: ID
+    }
+    name is required
+    if parent is null, site is attached as a root site
+
+  output
+    {
+      _id: ID,
+    	name: string,
+    	added_time: Date,
+    	note: string,
+    	parent: ID
+    	flowers: [flower],
+    	owner: ID
+    }
+*/
 const create = newSite =>  {
   const request = axios.post(baseUrl, newSite)
   return request.then(response => response.data)
 }
 
-const getSite = id =>  {
+/*
+  Get a site and its subsites
+  input
+    id: ID
+
+  output
+    {
+      site: {
+        _id: ID,
+      	name: string,
+      	added_time: Date,
+      	note: string,
+      	parent: ID
+      	flowers: [flower],
+      	owner: ID
+    	},
+    	subsites: [
+    	  {
+    	    _id: ID,
+    	    name: string,
+    	    note: string
+    	  } ]
+    }
+*/
+const get = id =>  {
   const request = axios.get(`${baseUrl}/${id}`)
   return request.then(response => response.data)
 }
 
-const getRootSites = () => {
+/*
+  Get all root sites (sites that have no parents)
+  output
+  [
+    {
+      _id: ID,
+    	name: string,
+    	added_time: Date,
+    	note: string,
+    	parent: ID
+    	flowers: [flower],
+    	owner: ID
+    }
+  ]
+  
+*/
+const getRoot = () => {
   const request = axios.get(baseUrl)
   return request.then(response => response.data)
 }
 
+/*
+  Remove site from database
+  input
+    id: ID
+
+  output
+    {
+      "Deleted count": int
+    }
+*/
 const remove = id => {
   return axios.delete(`${baseUrl}/${id}`)
 }
 
 export default {
-  create
+  create, getRoot, get, remove
 }
