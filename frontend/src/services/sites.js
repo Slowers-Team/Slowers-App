d2e63e8abb28d6778b505183b873be2ea80bbb3d
Halmela Mikko O <mikko.halmela@helsinki.fi>
2024-09-26 15:41:45 +0300

    create site-services

--- frontend/src/services/sites.js
+++ frontend/src/services/sites.js
@@ -1,9 +1,23 @@
 import axios from 'axios'
-const baseUrl = '/api/---'
+const baseUrl = '/api/sites'
 
 const create = newSite =>  {
   const request = axios.post(baseUrl, newSite)
   return request.then(response => response.data)
+}
+
+const getSite = id =>  {
+  const request = axios.get(`${baseUrl}/${id}`)
+  return request.then(response => response.data)
+}
+
+const getRootSites = () => {
+  const request = axios.get(baseUrl)
+  return request.then(response => response.data)
+}
+
+const remove = id => {
+  return axios.delete(`${baseUrl}/${id}`)
 }
 
 export default {
