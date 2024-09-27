faa668eeea22d6a30cb1330c310affacef8fa617
Halmela Mikko O <mikko.halmela@helsinki.fi>
2024-09-27 13:55:04 +0300

    add create and delete to SitePage

--- frontend/src/pages/SitePage.jsx
+++ frontend/src/pages/SitePage.jsx
@@ -1,10 +1,21 @@
+import { useState, useEffect } from 'react'
+
 import SiteService from '../services/sites'
 import NewSiteForm from '../components/NewSiteForm'
 import SiteFlexbox from '../components/SiteFlexbox'
 
 const SitePage = () => {
+    const [sites, setSites] = useState([])
+    const [showAddNewSite, setShowAddNewSite] = useState(false)
+  
+    useEffect(() => {
+      SiteService
+        .getRoot()
+        .then(initialSites => setSites(initialSites))
+    }, [])
 
-    const createNewSite = SiteObject => {
+
+    const createSite = SiteObject => {
         SiteService
             .create(SiteObject)
             .catch(error => {
@@ -12,9 +23,21 @@ const SitePage = () => {
             })
     }
 
+    const deleteSite = SiteObject => {
+      if (window.confirm(`Are you sure you want to delete site ${SiteObject.name} and its subsites?`)) {
+        SiteService
+          .remove(SiteObject._id)
+          .then(response => {
+            console.log(response)
+            setSites(l => l.filter(item => item._id !== SiteObject._id));
+          })
+      }
+    }
+
+
   return (
     <div>
-      <NewSiteForm createNewSite={createNewSite} />
+      <NewSiteForm createSite={createSite} />
       <SiteFlexbox />
     </div>
   )
