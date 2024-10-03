75d66d7d892f197e08a770c6ce2ec80790d639c6
Ville Sirviö <VSirvio@users.noreply.github.com>
2024-10-03 11:48:59 +0300

    Move the backend test data to 'testing'
    From: 'backend/main_flowers_test.go'
    To: 'backend/testing/testdata.go'
    The new functions for accessing the test data: 'GetTestFlowers', 'GetTestID'

--- backend/main_flowers_test.go
+++ backend/main_flowers_test.go
@@ -3,7 +3,6 @@ package main
 import (
 	"errors"
 	"testing"
-	"time"
 
 	"github.com/stretchr/testify/mock"
 
@@ -13,15 +12,8 @@ import (
 )
 
 func TestFlowersRoute(t *testing.T) {
-	testFlowers := []database.Flower{
-		{
-			Name: "sunflower",
-			LatinName: "Helianthus annuus",
-			AddedTime: time.Date(2024, 9, 30, 21, 11, 54, 0, time.UTC),
-		},
-	}
-
-	testID := "842af389e234e768923475bc"
+	testFlowers := sltest.GetTestFlowers()
+	testID := sltest.GetTestID()
 
 	tests := []sltest.TestCase{
 		{
---
+++ backend/testing/testdata.go
@@ -0,0 +1,21 @@
+package testing
+
+import (
+	"time"
+
+	"github.com/Slowers-team/Slowers-App/database"
+)
+
+func GetTestFlowers() []database.Flower {
+    return []database.Flower{
+		{
+			Name: "sunflower",
+			LatinName: "Helianthus annuus",
+			AddedTime: time.Date(2024, 9, 30, 21, 11, 54, 0, time.UTC),
+		},
+	}
+}
+
+func GetTestID() string {
+	return "842af389e234e768923475bc"
+}
