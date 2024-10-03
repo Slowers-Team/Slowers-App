6209343f4b4521ab5e86747df46311662a6c17f0
Ville Sirviö <VSirvio@users.noreply.github.com>
2024-10-03 12:06:13 +0300

    Split HTTP API unit tests into separate functions

--- backend/main_flowers_test.go
+++ backend/main_flowers_test.go
@@ -11,61 +11,59 @@ import (
 	"github.com/Slowers-team/Slowers-App/utils"
 )
 
-func TestFlowersRoute(t *testing.T) {
-	testFlowers := sltest.GetTestFlowers()
-	testID := sltest.GetTestID()
-
-	tests := []sltest.TestCase{
-		{
-			Description:   "\"GET /api/flowers\" without error",
-			Route:         "/api/flowers",
-			Method:        "GET",
-			Body:          "",
-			ExpectedError: false,
-			ExpectedCode:  200,
-			ExpectedBody:  utils.FlowersToJSON(testFlowers),
-			SetupMocks:    func(db *database.MockDatabase) {
-				db.On(
-					"GetFlowers", mock.Anything,
-				).Return(
-					testFlowers, nil,
-				).Once()
-			},
+func TestListingFlowersWithoutError(t *testing.T) {
+	sltest.RunTest(t, sltest.TestCase{
+		Description:   "\"GET /api/flowers\" without error",
+		Route:         "/api/flowers",
+		Method:        "GET",
+		Body:          "",
+		ExpectedError: false,
+		ExpectedCode:  200,
+		ExpectedBody:  utils.FlowersToJSON(sltest.GetTestFlowers()),
+		SetupMocks:    func(db *database.MockDatabase) {
+			db.On(
+				"GetFlowers", mock.Anything,
+			).Return(
+				sltest.GetTestFlowers(), nil,
+			).Once()
 		},
-		{
-			Description:   "\"GET /api/flowers\" with error",
-			Route:         "/api/flowers",
-			Method:        "GET",
-			Body:          "",
-			ExpectedError: false,
-			ExpectedCode:  500,
-			ExpectedBody:  "Database error",
-			SetupMocks:    func(db *database.MockDatabase) {
-				db.On(
-					"GetFlowers", mock.Anything,
-				).Return(
-					[]database.Flower{}, errors.New("Database error"),
-				).Once()
-			},
-		},
-		{
-			Description:   "DELETE /api/flowers/<id>",
-			Route:         "/api/flowers/" + testID,
-			Method:        "DELETE",
-			Body:          utils.IDToJSON(testID),
-			ExpectedError: false,
-			ExpectedCode:  204,
-			ExpectedBody:  "",
-			SetupMocks:    func(db *database.MockDatabase) {
-				db.On(
-					"DeleteFlower", mock.Anything, testID,
-				).Return(
-					true, nil,
-				).Once()
-			},
-		},
+	})
+}
 
-	}
+func TestListingFlowersWithError(t *testing.T) {
+	sltest.RunTest(t, sltest.TestCase{
+		Description:   "\"GET /api/flowers\" with error",
+		Route:         "/api/flowers",
+		Method:        "GET",
+		Body:          "",
+		ExpectedError: false,
+		ExpectedCode:  500,
+		ExpectedBody:  "Database error",
+		SetupMocks:    func(db *database.MockDatabase) {
+			db.On(
+				"GetFlowers", mock.Anything,
+			).Return(
+				[]database.Flower{}, errors.New("Database error"),
+			).Once()
+		},
+	})
+}
 
-	sltest.RunTests(t, tests)
+func TestDeletingFlower(t *testing.T) {
+	sltest.RunTest(t, sltest.TestCase{
+		Description:   "DELETE /api/flowers/<id>",
+		Route:         "/api/flowers/" + sltest.GetTestID(),
+		Method:        "DELETE",
+		Body:          utils.IDToJSON(sltest.GetTestID()),
+		ExpectedError: false,
+		ExpectedCode:  204,
+		ExpectedBody:  "",
+		SetupMocks:    func(db *database.MockDatabase) {
+			db.On(
+				"DeleteFlower", mock.Anything, sltest.GetTestID(),
+			).Return(
+				true, nil,
+			).Once()
+		},
+	})
 }
--- backend/testing/http.go
+++ backend/testing/http.go
@@ -1,7 +1,6 @@
 package testing
 
 import (
-	"fmt"
 	"io"
 	"net/http"
 	"strings"
@@ -25,39 +24,31 @@ type TestCase struct {
 	SetupMocks    func(db *database.MockDatabase)
 }
 
-func RunTests(t *testing.T, tests []TestCase) {
-	for i, test := range tests {
-		app := application.SetupAndSetAuthTo(false)
+func RunTest(t *testing.T, test TestCase) {
+	app := application.SetupAndSetAuthTo(false)
 
-		db := new(database.MockDatabase)
-		handlers.SetDatabase(db)
+	db := new(database.MockDatabase)
+	handlers.SetDatabase(db)
 
-		test.SetupMocks(db)
+	test.SetupMocks(db)
 
-		req, _ := http.NewRequest(test.Method, test.Route, strings.NewReader(test.Body))
+	req, _ := http.NewRequest(test.Method, test.Route, strings.NewReader(test.Body))
 
-		res, err := app.Test(req, -1)
+	res, err := app.Test(req, -1)
 
-		db.AssertExpectations(t)
+	db.AssertExpectations(t)
 
-		assert.Equalf(t, test.ExpectedError, err != nil, test.Description)
+	assert.Equalf(t, test.ExpectedError, err != nil, test.Description)
 
-		if test.ExpectedError {
-			continue
-		}
-
-		assert.Equalf(t, test.ExpectedCode, res.StatusCode, test.Description)
-
-		body, err := io.ReadAll(res.Body)
-
-		assert.Nilf(t, err, test.Description)
-
-		assert.Equalf(t, test.ExpectedBody, string(body), test.Description)
-
-		if t.Failed() {
-			fmt.Println("Test", i, ":", test.Description, ": FAIL")
-		} else {
-			fmt.Println("Test", i, ":", test.Description, ": PASS")
-		}
+	if test.ExpectedError {
+		return
 	}
+
+	assert.Equalf(t, test.ExpectedCode, res.StatusCode, test.Description)
+
+	body, err := io.ReadAll(res.Body)
+
+	assert.Nilf(t, err, test.Description)
+
+	assert.Equalf(t, test.ExpectedBody, string(body), test.Description)
 }
