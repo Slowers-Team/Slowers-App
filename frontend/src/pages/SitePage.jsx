5f7d19a71b225a04389912a61b35bf915059594d
Amanda <amanda.hamynen@gmail.com>
2024-10-03 15:53:07 +0300

    Remove unused line

--- frontend/src/pages/SitePage.jsx
+++ frontend/src/pages/SitePage.jsx
@@ -30,13 +30,10 @@ const SitePage = () => {
   }, [params.id, navigate])
 
   const addFlower = flowerObject => {
-    flowerService
-      .create(flowerObject)
-      .then(returnedFlower => setFlowers(flowers.concat(returnedFlower)))
-      .catch(error => {
-        console.log(error)
-        alert("Adding failed")
-      })
+    flowerService.create(flowerObject).catch(error => {
+      console.log(error)
+      alert("Adding failed")
+    })
   }
 
   const createSite = siteObject => {
