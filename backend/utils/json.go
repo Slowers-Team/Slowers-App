f1b4b9688e9e275839d388250c410c57311bc2f3
Ville Sirviö <VSirvio@users.noreply.github.com>
2024-10-04 19:48:07 +0300

    Add new conversion functions to JSON in backend

--- backend/utils/json.go
+++ backend/utils/json.go
@@ -4,6 +4,8 @@ import (
 	"encoding/json"
 	"log"
 
+	"go.mongodb.org/mongo-driver/bson"
+
 	"github.com/Slowers-team/Slowers-App/database"
 )
 
@@ -21,6 +23,46 @@ func FlowersToJSON(flowers []database.Flower) string {
 		log.Fatal(err)
 	}
 	return string(flowersJSON)
+}
+
+func SiteToJSON(site database.Site) string {
+	siteJSON, err := json.Marshal(site)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return string(siteJSON)
+}
+
+func SitesToJSON(sites []database.Site) string {
+	sitesJSON, err := json.Marshal(sites)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return string(sitesJSON)
+}
+
+func SiteDataToJSON(siteData bson.M) string {
+	siteDataJSON, err := json.Marshal(siteData)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return string(siteDataJSON)
+}
+
+func UserToJSON(user database.User) string {
+	userJSON, err := json.Marshal(user)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return string(userJSON)
+}
+
+func LogInToJSON(login database.LogIn) string {
+	loginJSON, err := json.Marshal(login)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return string(loginJSON)
 }
 
 func IDToJSON(id string) string {
