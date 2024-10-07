9a73b5480582fa1bc60cf2a87ba8f2370d0f36ef
Halmela Mikko O <mikko.halmela@helsinki.fi>
2024-10-07 15:55:37 +0300

    user can select role when registering

--- frontend/src/components/RegisterForm.jsx
+++ frontend/src/components/RegisterForm.jsx
@@ -4,18 +4,21 @@ const RegisterForm = ({ createNewUser }) => {
     const [newUsername, setNewUsername] = useState('')
     const [newPassword, setNewPassword] = useState('')
     const [newEmail, setNewEmail] = useState('')
+    const [newRole, setNewRole] = useState('')
 
     const addUser = event => {
         event.preventDefault()
         createNewUser({ 
             username: newUsername,
             password: newPassword,
-            email: newEmail 
+            email: newEmail,
+            role: newRole
         })
 
         setNewUsername('')
         setNewPassword('')
         setNewEmail('')
+        setNewRole('')
     }
 
     return (
@@ -32,6 +35,17 @@ const RegisterForm = ({ createNewUser }) => {
                 <div>
                     <label htmlFor="newEmailInput">Email:</label>
                     <input type="email" id="newEmailInput" value={newEmail} onChange={event => setNewEmail(event.target.value)} />
+                </div>
+                <div>
+                    <label htmlFor="roleSelector">Default role:</label>
+                    <div>
+                        <input type="radio" name="roleSelector" id="growerSelector" value="grower" onChange={event => setNewRole(event.target.value)}/>
+                        <label htmlFor="growerSelector">Grower</label>
+                    </div>
+                    <div>
+                        <input type="radio" name="roleSelector" id="retailerSelector" value="retailer" onChange={event => setNewRole(event.target.value)}/>
+                        <label htmlFor="retailerSelector">Retailer</label>
+                    </div>
                 </div>
                 <div>
                     <button id="createNewUserButton" type="submit">Register</button>
