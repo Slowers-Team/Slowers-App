1f48dd02c87d4d87b4906615808e374f37781858
Amanda <amanda.hamynen@gmail.com>
2024-10-07 06:57:32 +0300

    Update frontend to include the site name in flowerlist for growers

--- frontend/src/components/FlowerList.jsx
+++ frontend/src/components/FlowerList.jsx
@@ -6,6 +6,7 @@ const FlowerList = ({ flowers, deleteFlower }) => {
           <th>Name</th>
           <th>Latin name</th>
           <th>Added time</th>
+          <th>Site</th>
         </tr>
       </thead>
       <tbody>
@@ -13,17 +14,22 @@ const FlowerList = ({ flowers, deleteFlower }) => {
           let addedTime = new Date(flower.added_time)
 
           let date = addedTime.toLocaleDateString('fi')
-          let hour = addedTime.toLocaleString('fi', {hour: 'numeric'})
-          let minute = addedTime.toLocaleString('fi', {minute: '2-digit'})
+          let hour = addedTime.toLocaleString('fi', { hour: 'numeric' })
+          let minute = addedTime.toLocaleString('fi', { minute: '2-digit' })
           let addedTimeStr = `${date} ${hour}:${minute}`
 
           return (
             <tr key={flower._id}>
-              <td>{ flower.name }</td>
-              <td><em>{ flower.latin_name }</em></td>
-              <td>{ addedTimeStr }</td>
+              <td>{flower.name}</td>
               <td>
-                <button id="deleteFlowerButton" onClick={() => deleteFlower(flower)}>Delete</button>
+                <em>{flower.latin_name}</em>
+              </td>
+              <td>{addedTimeStr}</td>
+              <td>{flower.site_name}</td>
+              <td>
+                <button id="deleteFlowerButton" onClick={() => deleteFlower(flower)}>
+                  Delete
+                </button>
               </td>
             </tr>
           )
