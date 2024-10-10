0a24a7588ed07406f7839dd989508d36205ddcfd
Sanni Isokirmo <sanni.isokirmo@helsinki.fi>
2024-10-11 02:03:50 +0300

    Update log in page

--- frontend/src/pages/LogInPage.jsx
+++ frontend/src/pages/LogInPage.jsx
@@ -9,9 +9,19 @@ const LogInPage = ({ setIsLoggedIn }) => {
   };
 
   return (
-    <div>
-      <LogIn onLogin={handleLogin} setIsLoggedIn={setIsLoggedIn} />
-    </div>
+    <section className='vh-100' style = {{ backgroundColor: '#eee' }}>
+      <div className='container py-5 h-100'>
+        <div className='row d-flex justify-content-center align-items-center h-100'>
+          <div className='col-12 col-md-8 col-lg-6 col-xl-5'>
+            <div className="card shadow-2-strong" style={{ borderRadius: '1rem' }} data-mdb-ripple-color="light">
+              <div className='card-body p-5 text-center'>
+                <LogIn onLogin={handleLogin} setIsLoggedIn={setIsLoggedIn} />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
   );
 };
 
