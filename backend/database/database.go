d4dfc320544945d7cc105bfe6b52b51382544a29
NUn <nun.lukkarinen01@gmail.com>
2024-10-10 12:10:37 +0300

    Tried to add database to interface

--- backend/database/database.go
+++ backend/database/database.go
@@ -22,6 +22,7 @@ type Database interface {
 
 	GetFlowers(ctx context.Context) ([]Flower, error)
 	GetUserFlowers(ctx context.Context, userID ObjectID) ([]Flower, error)
+	GetAllFlowersFromSite(ctx context.Context, userID ObjectID, siteID ObjectID) ([]Flower, error)
 	AddFlower(ctx context.Context, newFlower Flower) (*Flower, error)
 	DeleteFlower(ctx context.Context, id string) (bool, error)
 
