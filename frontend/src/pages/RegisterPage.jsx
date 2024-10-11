8beb152e77c84aab007ac5163027fcf64d2ed26f
Sanni Isokirmo <sanni.isokirmo@helsinki.fi>
2024-10-11 03:09:28 +0300

    Update register page

--- frontend/src/pages/RegisterPage.jsx
+++ frontend/src/pages/RegisterPage.jsx
@@ -12,9 +12,20 @@ const RegisterPage = () => {
     }
 
   return (
-    <div>
-      <RegisterForm createNewUser={createNewUser} />
-    </div>
+    <section className='vh-100' style = {{ backgroundColor: '#eee' }}>
+      <div className='container py-5 h-100'>
+        <div className='row d-flex justify-content-center align-items-center h-100'>
+          <div className='col-12 col-md-8 col-lg-6 col-xl-5'>
+            <div className="card" style={{ borderRadius: '1rem' }}>
+              <div className='card-body p-5'>
+                <h2 className='mb-5 text-center'>Slowers registration</h2>
+                <RegisterForm createNewUser={createNewUser} />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
   )
 }
 
