a9f318c0436a4747d391af1bd2ca7612ffd073fb
Sanni Isokirmo <sanni.isokirmo@helsinki.fi>
2024-10-13 06:40:48 +0300

    Update register form roles and terms

--- frontend/src/components/RegisterForm.jsx
+++ frontend/src/components/RegisterForm.jsx
@@ -27,6 +27,8 @@ const RegisterForm = ({ createNewUser }) => {
         setNewEmail('')
         setNewRole('')
         setTermsAccepted(false)
+
+        alert(t('alert.usercreated'))
     }
 
     return (
@@ -37,27 +39,25 @@ const RegisterForm = ({ createNewUser }) => {
                   <input id="newUsernameInput" value={newUsername} placeholder={t('user.input.username')} onChange={event => setNewUsername(event.target.value)} className='form-control' required/>
                 </div>
                 <div className='form-group mb-4'>
+                  <label htmlFor="newEmailInput">{t('user.data.email')}</label>
+                  <input type="email" id="newEmailInput" value={newEmail} placeholder={t('user.input.email')} onChange={event => setNewEmail(event.target.value)} className='form-control' required/>
+                </div>
+                <div className='form-group mb-4'>
                   <label htmlFor="newPasswordInput">{t('user.data.password')}</label>
                   <input type="password" id="newPasswordInput" value={newPassword} placeholder={t('user.input.password')} onChange={event => setNewPassword(event.target.value)} className='form-control' required/>
                 </div>
                 <div className='form-group mb-4'>
-                  <label htmlFor="newEmailInput">{t('user.data.email')}</label>
-                  <input type="email" id="newEmailInput" value={newEmail} placeholder={t('user.input.email')} onChange={event => setNewEmail(event.target.value)} className='form-control' required/>
-                </div>
-                <div>
                     <label htmlFor="roleSelector">{t('label.defaultrole')}:</label>
                     <div>
-                      <input type="radio" name="roleSelector" id="growerSelector" value="grower" onChange={event => setNewRole(event.target.value)}/>
-                      <label htmlFor="growerSelector">{t('role.grower')}</label>
-                    </div>
-                    <div>
-                      <input type="radio" name="roleSelector" id="retailerSelector" value="retailer" onChange={event => setNewRole(event.target.value)}/>
-                      <label htmlFor="retailerSelector">{t('role.retailer')}</label>
+                      <input type="radio" className='btn-check' name="roleSelector" id="growerSelector" autoComplete="off" value="grower" onChange={event => setNewRole(event.target.value)} required />
+                      <label className='btn btn-light' htmlFor="growerSelector" style={{ marginRight: "0.5rem" }}>{t('role.grower')}</label>
+                      <input type="radio" className='btn-check' name="roleSelector" id="retailerSelector" autoComplete='off' value="retailer" onChange={event => setNewRole(event.target.value)} />
+                      <label className='btn btn-light' htmlFor="retailerSelector"style={{ marginRight: "0.5rem" }}>{t('role.retailer')}</label>
                     </div>
                 </div>
-                <div>
-                  <input type='checkbox' id='termsCheckbox' checked={termsAccepted} onChange={() => setTermsAccepted(!termsAccepted)}/>
-                  <label htmlFor="termsCheckbox">{t('label.iagreeto')} <a href='/terms' target="_blank" rel="noopener noreferrer">{t('label.terms')}</a></label>
+                <div className='form-check form-group mb-4'>
+                  <input type='checkbox' className='form-check-input' id='termsCheckbox' checked={termsAccepted} onChange={() => setTermsAccepted(!termsAccepted)}/>
+                  <label className='form-check-label' htmlFor="termsCheckbox">{t('label.iagreeto')} <a href='/terms' target="_blank" rel="noopener noreferrer">{t('label.terms')}</a></label>
                 </div>
                 <div>
                   <button type="submit" id="createNewUserButton" className='btn btn-primary'>{t('button.register')}</button>
