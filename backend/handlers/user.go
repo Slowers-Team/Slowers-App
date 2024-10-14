8639a3e11f60eff965548dd234e0cb011724eae9
Halmela Mikko O <mikko.halmela@helsinki.fi>
2024-10-14 15:29:27 +0300

    convert role switcher function to use user-fetcher

--- backend/handlers/user.go
+++ backend/handlers/user.go
@@ -111,14 +111,10 @@ func GetUser(c *fiber.Ctx) error {
 }
 
 func SetRole(c *fiber.Ctx) error {
-	user, ok := c.Locals("userID").(string)
-	if !ok {
-		return c.Status(500).SendString("Invalid userID in header")
+	userID, err := GetCurrentUser(c)
+	if err != nil {
+		return c.Status(500).SendString(err.Error())
 	}
-	if !database.IsValidID(user) {
-		return c.Status(500).SendString("Malformed userID in header")
-	}
-	userID := database.NewID(user)
 
 	var role string
 	if err := c.BodyParser(&role); err != nil {
@@ -128,7 +124,7 @@ func SetRole(c *fiber.Ctx) error {
 		return c.Status(500).SendString("Role must be grower or retailer")
 	}
 
-	err := db.SetUserRole(c.Context(), userID, role)
+	err = db.SetUserRole(c.Context(), *userID, role)
 	if err != nil {
 		return c.Status(500).SendString(err.Error())
 	}
