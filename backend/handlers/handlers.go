ec3a5af65fcc097c304b43cd6352d3370baae0d9
Halmela Mikko O <mikko.halmela@helsinki.fi>
2024-10-14 15:28:36 +0300

    create function for fetching local user

--- backend/handlers/handlers.go
+++ backend/handlers/handlers.go
@@ -1,9 +1,27 @@
 package handlers
 
-import "github.com/Slowers-team/Slowers-App/database"
+import (
+	"fmt"
+
+	"github.com/Slowers-team/Slowers-App/database"
+	"github.com/gofiber/fiber/v2"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
 
 var db database.Database
 
 func SetDatabase(newDb database.Database) {
 	db = newDb
+}
+
+func GetCurrentUser(c *fiber.Ctx) (*primitive.ObjectID, error) {
+	id, ok := c.Locals("userID").(string)
+	if !ok {
+		return nil, fmt.Errorf("userID not set in local storage")
+	}
+	userID, err := database.ParseID(id)
+	if err != nil {
+		return nil, err
+	}
+	return userID, nil
 }
