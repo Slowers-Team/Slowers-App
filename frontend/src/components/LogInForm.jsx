4396ae139e1f797590e3161b8e818349693e695f
Ville Sirviö <VSirvio@users.noreply.github.com>
2024-10-15 22:29:14 +0300

    Restore 'setDefaultRole' prop in LogInForm.jsx

--- frontend/src/components/LogInForm.jsx
+++ frontend/src/components/LogInForm.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import userService from '../services/users'
 import { useTranslation } from 'react-i18next'
 
-const LogIn = ({ onLogin, setIsLoggedIn }) => {
+const LogIn = ({ onLogin, setIsLoggedIn, setDefaultRole }) => {
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
   const [error, setError] = useState('')
