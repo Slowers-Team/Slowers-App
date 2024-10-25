afd7fa5da8f4aadb0c30d1146750229410e98ccc
Amanda <amanda.hamynen@gmail.com>
2024-10-25 11:03:41 +0300

    Fix a bug that shows minutes as single digit if it is less than 10

--- frontend/src/components/FlowerModal.jsx
+++ frontend/src/components/FlowerModal.jsx
@@ -15,9 +15,8 @@ const FlowerModal = ({ show, handleClose, flower, deleteFlower }) => {
     let addedTime = new Date(flower.added_time)
 
     let date = addedTime.toLocaleDateString('fi')
-    let hour = addedTime.toLocaleString('fi', { hour: 'numeric' })
-    let minute = addedTime.toLocaleString('fi', { minute: '2-digit' })
-    let addedTimeStr = `${date} ${hour}:${minute}`
+    let time = addedTime.toLocaleTimeString('fi', { hour: '2-digit', minute: '2-digit' })
+    let addedTimeStr = `${date} ${time}`
 
     return addedTimeStr
   }
