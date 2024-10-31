1406594b5e41eaa07a8c3c8399004133e1cf3fe3
Halmela Mikko O <mikko.halmela@helsinki.fi>
2024-10-31 14:35:31 +0200

    test image upload

---
+++ frontend/tests/services/images.test.js
@@ -0,0 +1,38 @@
+import images from '../../src/services/images'
+import tokenService from '../../src/services/token'
+import testImage from '../testdata/testImage.png'
+import { expect, vi } from 'vitest'
+import axios from 'axios'
+
+vi.mock('axios')
+
+tokenService.fetchToken = vi.fn().mockReturnValue('faketoken')
+
+test('creates a new image correctly', async() => {
+    const newImage = {
+        entity: '66f5027d6430d371f8636c3c',
+        note: 'this is a note',
+        image: testImage
+    }
+
+    const config = {
+        headers: { 
+            Authorization: tokenService.fetchToken(),
+            'Content-Type': 'multipart/form-data'
+        },
+    }
+
+
+    const expected = {
+        entity: newImage.entity,
+        file_format: "png",
+        note: 'this is a note',
+    }
+
+    axios.post.mockResolvedValue({ data: expected })
+
+    const result = await images.create(newImage)
+
+    expect(result).toMatchObject(expected)
+    expect(axios.post).toHaveBeenCalledWith('/api/images', newImage, config)
+})
