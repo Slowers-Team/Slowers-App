7015a12a7c6c1d4521b1cd51bfae8ee91e240cab
Sanni Isokirmo <sanni.isokirmo@helsinki.fi>
2024-11-01 03:04:08 +0200

    Create base tab bar

--- frontend/src/layouts/GrowerLayout.jsx
+++ frontend/src/layouts/GrowerLayout.jsx
@@ -1,15 +1,40 @@
-import { Outlet, NavLink } from 'react-router-dom'
+import { Outlet, NavLink, Link } from 'react-router-dom'
 //import './Grower.css'
 import { useTranslation } from 'react-i18next'
+import { Nav } from 'react-bootstrap'
 
-
+const tabBar = () => {
+  const { t, i18n } = useTranslation()
+  return (
+    <div className='my-2'>
+      <Link to="/grower" className="mx-2 text-secondary text-decoration-none">Placeholder</Link>
+      <div className="my-3">
+        <Nav variant='tabs' defaultActiveKey="/grower">
+          <Nav.Item>
+            <Nav.Link className="text-success" as={NavLink} to="/"> 
+              {t('menu.home')}
+            </Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link className="text-success" as={NavLink} to="/grower/flowers">
+              {t('menu.flowers')}
+            </Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link className="text-success" as={NavLink} to="/grower">
+              Subsites
+            </Nav.Link>
+          </Nav.Item>
+        </Nav>
+      </div>
+    </div>
+  )
+}
 const GrowerLayout = () => {
   const { t, i18n } = useTranslation()
   return (
     <div>
-      <header>
-        <h2>{t('menu.grower')}</h2>
-      </header>
+      {tabBar()}
       <div>
         <main className="main-container">
           <Outlet />
