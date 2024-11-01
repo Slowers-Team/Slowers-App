9d5b5a8d8355dbcb7acf8c28a6a0175a01b67351
Sanni Isokirmo <sanni.isokirmo@helsinki.fi>
2024-11-01 02:14:30 +0200

    Remove old styling from grower page

--- frontend/src/layouts/GrowerLayout.jsx
+++ frontend/src/layouts/GrowerLayout.jsx
@@ -2,38 +2,15 @@ import { Outlet, NavLink } from 'react-router-dom'
 //import './Grower.css'
 import { useTranslation } from 'react-i18next'
 
-const navigationBar = () => {
-  const { t, i18n } = useTranslation()
-  return (
-    <div className="nav-container">
-      <nav>
-        <ul>
-          <li>
-            <NavLink to="/grower" end>
-              {t('menu.home')}
-            </NavLink>
-          </li>
-          <li>
-            <NavLink to="/grower/flowers">{t('menu.flowers')}</NavLink>
-          </li>
-        </ul>
-      </nav>
-    </div>
-  )
-}
 
 const GrowerLayout = () => {
   const { t, i18n } = useTranslation()
   return (
-    <div className="layout-container">
-      <header className="header">
+    <div>
+      <header>
         <h2>{t('menu.grower')}</h2>
       </header>
-      <div className="content">
-        <aside className="side-container">
-          <h2>{t('title.navigation')}</h2>
-          {navigationBar()}
-        </aside>
+      <div>
         <main className="main-container">
           <Outlet />
         </main>
