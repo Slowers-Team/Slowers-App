8378c9943c1f19406b6198b47ce76fa6e9061194
Amanda <amanda.hamynen@gmail.com>
2024-11-02 11:45:07 +0200

    Move NavigationBar into its own component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -12,11 +12,10 @@ import GrowerLayout from './layouts/GrowerLayout'
 import GrowerHomePage from './pages/GrowerHomePage'
 import GrowerFlowerPage from './pages/GrowerFlowerPage'
 import GrowerSitesPage from './pages/GrowerSitesPage'
-import LangSelect from './components/LangSelect'
-import { BrowserRouter as Router, Routes, Route, Link, Navigate } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
 import { useState, useEffect } from 'react'
 import { useTranslation } from 'react-i18next'
-import { Navbar, Nav, NavDropdown } from 'react-bootstrap'
+import NavigationBar from './NavigationBar'
 
 const App = () => {
   const [isLoggedIn, setIsLoggedIn] = useState(false)
@@ -55,57 +54,8 @@ const App = () => {
     <div>
       <Router>
         <div>
-          <Navbar collapseOnSelect expand="lg" bg="light">
-            <Navbar.Brand>
-              <h1 className="mx-3 text-center">Slowers</h1>
-            </Navbar.Brand>
-            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-            <Navbar.Collapse id="responsive-navbar-nav">
-              <Nav className="me-auto justify-content-start">
-                <Nav.Link as={Link} to="/">
-                  {t('menu.home')}
-                </Nav.Link>
-                {!isLoggedIn && (
-                  <Nav.Link as={Link} to="/login">
-                    {t('menu.login')}
-                  </Nav.Link>
-                )}
-                {!isLoggedIn && (
-                  <Nav.Link as={Link} to="/register">
-                    {t('menu.register')}
-                  </Nav.Link>
-                )}
-                {isLoggedIn && (
-                  <NavDropdown title={t('menu.role')} id="collasible-nav-dropdown">
-                    <Nav.Link as={Link} to="/retailer">
-                      {t('menu.retailer')}
-                    </Nav.Link>
-                    <Nav.Link as={Link} to="/grower">
-                      {t('menu.grower')}
-                    </Nav.Link>
-                  </NavDropdown>
-                )}
-                {isLoggedIn && (
-                  <Nav.Link as={Link} onClick={handleLogout}>
-                    {t('menu.logout')}
-                  </Nav.Link>
-                )}
-              </Nav>
-              <Nav className="ms-auto">
-                {isLoggedIn && (
-                  <Nav.Link as={Link} to="/user">
-                    {t('menu.profile')}
-                  </Nav.Link>
-                )}
-                <Nav.Link as={Link} to="/terms">
-                  {t('menu.terms')}
-                </Nav.Link>
-                <NavDropdown title={t('menu.language')} id="collasible-nav-dropdown">
-                  <LangSelect />
-                </NavDropdown>
-              </Nav>
-            </Navbar.Collapse>
-          </Navbar>
+
+          <NavigationBar isLoggedIn={isLoggedIn} handleLogout={handleLogout}/>
 
           <Routes>
             <Route element={<ProtectedRoute isLoggedIn={isLoggedIn} />}>
