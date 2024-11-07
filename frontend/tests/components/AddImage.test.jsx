afcd0bfc09b64e16566e7e66c43f4dbfac5ce9da
Ville Sirviö <VSirvio@users.noreply.github.com>
2024-11-07 23:12:59 +0200

    Remove unused imports from AddImage.test.jsx
    Co-authored-by: Amanda <55439398+amandahamynen@users.noreply.github.com>

--- frontend/tests/components/AddImage.test.jsx
+++ frontend/tests/components/AddImage.test.jsx
@@ -1,6 +1,5 @@
 import { render, screen } from '@testing-library/react'
 import AddImage from '../../src/components/image/AddImage'
-import { expect, vi } from 'vitest'
 import userEvent from '@testing-library/user-event'
 
 test('renders add image button', () => {
