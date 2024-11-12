aed3d0b1e79cad3a7ac5e176e275ecddd408b5a7
Amanda <amanda.hamynen@gmail.com>
2024-11-12 20:34:15 +0200

    Add E2E tests for registration and login

--- frontend/cypress/e2e/slowers_app.cy.js
+++ frontend/cypress/e2e/slowers_app.cy.js
@@ -3,4 +3,27 @@ describe('Slowers ', function() {
     cy.visit('http://localhost:5173')
     cy.contains('Slowers')
   })
+
+  it('register page can be opened', function() {
+    cy.visit('http://localhost:5173/register')
+    cy.contains('Slowers registration')
+  })
+
+  it('user can register', function() {
+    cy.visit('http://localhost:5173/register')
+    cy.get('#newUsernameInput').type('testuser')
+    cy.get('#newEmailInput').type('test@email.com')
+    cy.get('#newPasswordInput').type('testpassword')
+    cy.contains('Grower').click()
+    cy.get('#termsCheckbox').check()
+    cy.get('#createNewUserButton').click()
+  })
+
+  it('user can login', function() {
+    cy.visit('http://localhost:5173/login')
+    cy.get('#emailInput').type('test@email.com')
+    cy.get('#passwordInput').type('testpassword')
+    cy.get('#loginButton').click()
+    cy.contains('Homepage')
+  })
 })
