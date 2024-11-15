d74b5bd745d28d3dced46e547de81024166f76c7
Amanda <amanda.hamynen@gmail.com>
2024-11-15 11:38:09 +0200

    Add commands for register and login for e2e tests
    Co-authored-by: Ville Sirviö <VSirvio@users.noreply.github.com>

--- frontend/cypress/support/commands.js
+++ frontend/cypress/support/commands.js
@@ -1,25 +1,20 @@
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+Cypress.Commands.add('register' , ( {username, email, password, role} ) => {
+    cy.request('POST', '/api/register', {
+        username,
+        email,
+        password,
+        role
+    })
+})
+
+Cypress.Commands.add('login' , ( {email, password} ) => {
+    cy.visit('/login')
+    cy.get('#emailInput').type(email)
+    cy.get('#passwordInput').type(password)
+    cy.get('#loginButton').click()
+})
+
+Cypress.Commands.add('registerAndLogin' , ( {username, email, password, role} ) => {
+    cy.register({username, email, password, role})
+    cy.login({email, password})
+})
