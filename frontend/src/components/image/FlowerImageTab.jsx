269977001bc7d2dcc7ee1f47c94f3022cc892312
Eoyie <sanni.isokirmo@gmail.com>
2024-11-23 23:40:59 +0200

    Add delete functionality to delete icon

--- frontend/src/components/image/FlowerImageTab.jsx
+++ frontend/src/components/image/FlowerImageTab.jsx
@@ -1,9 +1,11 @@
 import { useEffect, useState } from 'react'
+import { useTranslation } from 'react-i18next'
 import ImageService from '../../services/images'
 import AddImage from './AddImage'
 import FlowerImageGallery from './FlowerImageGallery'
 
 const FlowerImageTab = ({ isGrower, flower }) => {
+    const { t } = useTranslation()
     const [images, setImages] = useState([])
 
     useEffect(() => {
@@ -20,7 +22,22 @@ const FlowerImageTab = ({ isGrower, flower }) => {
     }
 
     const deleteImage = imageObject => {
-        console.log("Deleting image:")
+      console.log("Deleting image:", imageObject) 
+      if (!imageObject || !imageObject._id) {
+        console.error("Image object is undefined or missing id")
+        return
+      }
+      if (window.confirm(`${t("Confirm image deletion")}?`)) {
+        ImageService.deleteImage(imageObject._id)
+          .then(() => {
+            setImages(l => l.filter(item => item._id !== imageObject._id))
+            alert(t("Image deleted"))
+          })
+          .catch(error => {
+            console.error('Error deleting image:', error)
+            alert(t("Error"))
+          })
+      }
     }
 
     const onImageUpload = () => {
