437030f9c9ad4f59f8788fa3e6e0a455c5e52097
Halmela Mikko O <mikko.halmela@helsinki.fi>
2024-11-26 14:32:40 +0200

    growerhomepage uses loader data

--- frontend/src/pages/GrowerHomePage.jsx
+++ frontend/src/pages/GrowerHomePage.jsx
@@ -1,22 +1,13 @@
-import { useParams } from 'react-router-dom'
-import { useState, useEffect } from 'react'
 import { useTranslation } from 'react-i18next'
-import siteService from '../services/sites'
+import { useRouteLoaderData } from 'react-router-dom' 
 
 const GrowerHomePage = () => {
-  const params = useParams()
-  const [site, setSite] = useState()
   const { t, i18n } = useTranslation()
-
-  useEffect(() => {
-    if (params.siteId) {
-      siteService.get(params.siteId).then(initialSite => setSite(initialSite.site))
-    }
-  }, [])
+  const { site } = useRouteLoaderData("site")
 
   return (
     <>
-      {params.siteId ? (
+      {site ? (
         <h2>{site?.name} {t('title.sitehome')} </h2>
       ) : (
         <h2>{t('title.home')}</h2>
