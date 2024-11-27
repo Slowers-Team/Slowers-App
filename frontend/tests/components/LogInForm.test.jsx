faa2f4f3dc8fd2a21a3a5e64a424eaffe74764bf
Halmela Mikko O <mikko.halmela@helsinki.fi>
2024-11-27 19:42:27 +0200

    fix broken loginformtests

--- frontend/tests/components/LogInForm.test.jsx
+++ frontend/tests/components/LogInForm.test.jsx
@@ -2,11 +2,13 @@ import { render, screen } from '@testing-library/react'
 import LogInForm from '../../src/components/LogInForm'
 import { expect, vi } from 'vitest'
 import userEvent from '@testing-library/user-event'
+import { RouterProvider, createMemoryRouter } from 'react-router-dom'
 
 test('renders LogIn form with email and password inputs', () => {
-    const login = vi.fn()
-
-    render(<LogInForm login={login} />)
+    const router = createMemoryRouter([
+        { path: "/login", element: <LogInForm />, action() {return {invalidLogin: true}} }
+    ], {initialEntries: ["/login"]} )
+    render(<RouterProvider router={router}/>)
 
     const email = screen.getByLabelText('Email')
     const password = screen.getByLabelText('Password')
@@ -14,10 +16,12 @@ test('renders LogIn form with email and password inputs', () => {
 })
 
 test('updates input values when typing', async () => {
-    const login = vi.fn()
     const user = userEvent.setup()
 
-    render(<LogInForm login={login} />)
+    const router = createMemoryRouter([
+        { path: "/login", element: <LogInForm />, action() {return {invalidLogin: true}} }
+    ], {initialEntries: ["/login"]} )
+    render(<RouterProvider router={router}/>)
 
     const emailInput = screen.getByLabelText('Email')
     const passwordInput = screen.getByLabelText('Password')
@@ -30,10 +34,13 @@ test('updates input values when typing', async () => {
 })
 
 test('does not clear input values after submit if email does not match standard format', async() => {
-    const login = vi.fn()
     const user = userEvent.setup()
 
-    render(<LogInForm login={login} />)
+    const router = createMemoryRouter([
+        { path: "/login", element: <LogInForm />, action() {return {invalidLogin: true}} }
+    ], {initialEntries: ["/login"]} )
+    render(<RouterProvider router={router}/>)
+
 
     const emailInput = screen.getByLabelText('Email')
     const passwordInput = screen.getByLabelText('Password')
