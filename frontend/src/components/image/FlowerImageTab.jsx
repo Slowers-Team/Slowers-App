f3321e5db12e3d251ecb6a9714824f777227391e
Eoyie <sanni.isokirmo@gmail.com>
2024-12-09 20:42:24 +0200

    Alerts for deleting last photo

--- frontend/src/components/image/FlowerImageTab.jsx
+++ frontend/src/components/image/FlowerImageTab.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react'
 import { useTranslation } from 'react-i18next'
 import ImageService from '../../services/images'
+import FlowerService from '../../services/flowers'
 import AddImage from './AddImage'
 import ImageGallery from './ImageGallery'
 
@@ -43,6 +44,23 @@ const FlowerImageTab = ({ isGrower, flower, updateFlower }) => {
         console.error("Image object is undefined or missing id")
         return
       }
+      if (images.length === 1) {
+        let confirmMessage = `${t('image.confirmimagedeletion')}?`
+        if (flower.visible) {
+            confirmMessage = `${t('image.flowerwillbehidden')}: ` + confirmMessage
+        }
+        if (window.confirm(confirmMessage)) {
+          if (flower.visible) {
+          FlowerService.toggleVisibility(flower._id).then(() => {updateFlower({...flower, visible: false})})
+          }
+          ImageService.deleteImage(imageObject._id).then(() => {setImages([])})
+          .catch(error => {
+            console.error('Error deleting image:', error)
+            alert(t('error.erroroccured'))
+          })
+        }
+        return
+      }
       if (window.confirm(`${t('image.confirmimagedeletion')}?`)) {
         ImageService.deleteImage(imageObject._id)
         .then(() => {
@@ -54,7 +72,7 @@ const FlowerImageTab = ({ isGrower, flower, updateFlower }) => {
           } else {
               favoriteImage(flower.favorite_image)
           }
-      })
+        })
           .catch(error => {
             console.error('Error deleting image:', error)
             alert(t('error.erroroccured'))
