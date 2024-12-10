a6215b63afbdf11812ce4f7cd4afaf18d227f1b4
Halmela Mikko O <mikko.halmela@helsinki.fi>
2024-12-10 14:50:46 +0200

    flower visibility can be set only if flower has a favorite image

--- frontend/src/components/FlowerModal.jsx
+++ frontend/src/components/FlowerModal.jsx
@@ -9,6 +9,8 @@ const FlowerModal = ({ show, handleClose, flower, deleteFlower, updateFlower, mo
   const { t } = useTranslation()
   const [isModifyFormVisible, setIsModifyFormVisible] = useState(false)
 
+  console.log(flower)
+
   const handleFlowerDelete = (flower) => {
     if (deleteFlower) {
       deleteFlower(flower)
@@ -70,7 +72,7 @@ const FlowerModal = ({ show, handleClose, flower, deleteFlower, updateFlower, mo
               <p>{t('flower.visible.long')}: {flower.visible 
                     ? t('flower.visible.true') 
                     : t('flower.visible.false')}
-              <VisibilityButton flower={flower} updateFlower={updateFlower}/>
+              {flower?.favorite_image ? <VisibilityButton flower={flower} updateFlower={updateFlower}/> : <></>}
               </p> : <></>}
               {deleteFlower && (
                 <button className="flower-button" onClick={() => handleFlowerDelete(flower)}>
