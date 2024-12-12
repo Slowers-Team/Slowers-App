b5acd56f1af367969b2ee68c288aab1bcb20cf68
NUn <nun.lukkarinen01@gmail.com>
2024-12-12 12:29:55 +0200

    Updated to newest version

--- frontend/src/components/FlowerModal.jsx
+++ frontend/src/components/FlowerModal.jsx
@@ -19,41 +19,7 @@ const FlowerModal = ({ show, handleClose, flower, deleteFlower, updateFlower, mo
         <Tabs defaultActiveKey="info" className="mb-3" mountOnEnter={true} unmountOnExit={true}>
           <Tab eventKey="info" title={t('menu.info')}>
             <div>
-              {isGrower && isModifyFormVisible ? (
-                  <div>
-                    <ModifyFlowerForm 
-                      flower={flower} 
-                      modifyFlower={modifyFlower} 
-                      handleFlowerModify={updateFlower}
-                      handleFormVisibility={handleFormVisibility}
-                    />
-                  </div> 
-                ) : (
-                  <div>
-                    <h3>{t('menu.info')}</h3>
-                    <p>{t('flower.data.name')}: {flower.name}</p>
-                    <p>{t('flower.data.latinname')}: {flower.latin_name}</p>
-                    <p>{t('flower.data.addedtime')}: {addedTime(flower)}</p>
-                    <p>{t('flower.data.site')}: {flower.site_name}</p>
-                    <p>{t('flower.data.qty')}: {flower.quantity}</p>
-                  </div>
-                )}
-              {isGrower ?
-              <p>{t('flower.visible.long')}: {flower.visible 
-                    ? t('flower.visible.true') 
-                    : t('flower.visible.false')}
-              {flower?.favorite_image ? <VisibilityButton flower={flower} updateFlower={updateFlower}/> : <></>}
-              </p> : <></>}
-              {deleteFlower && (
-                <button id="deleteFlowerButton" onClick={() => handleFlowerDelete(flower)}>
-                  <i className="bi bi-trash3-fill"></i>
-                </button>
-              )}
-              {isGrower && !isModifyFormVisible && (
-                <button id="modifyFlowerButton" onClick={handleFormVisibility}>
-                  <i className="bi bi-pencil-fill"> </i>
-                </button>
-              )}
+              <FlowerInfoTab isGrower={isGrower} flower={flower} deleteFlower={deleteFlower} updateFlower={updateFlower} modifyFlower={modifyFlower} handleClose={handleClose}/>
             </div>
           </Tab>
           <Tab eventKey="images" title={t('menu.images')}>
@@ -63,11 +29,6 @@ const FlowerModal = ({ show, handleClose, flower, deleteFlower, updateFlower, mo
           </Tab>
         </Tabs>
       </Modal.Body>
-      <Modal.Footer>
-        <Button variant="secondary" onClick={handleClose}>
-          {t('button.close')}
-        </Button>
-      </Modal.Footer>
     </Modal>
   )
 }
