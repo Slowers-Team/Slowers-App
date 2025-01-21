8edfb2f40977e263162f097fa6450ecaa071ac18
Sandra Sillanpää <sandra.sillanpaa@helsinki.fi>
2025-01-21 14:05:50 +0200

    fixed six vulnerabilities

---
+++ backend/mongo.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose')
+
+if (process.argv.length<3) {
+  console.log('give password as argument')
+  process.exit(1)
+}
+
+const password = process.argv[2]
+
+const url =
+  `mongodb+srv://sansilla:Aaltonen69@cluster0.4cfl7.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
+
+mongoose.set('strictQuery', false)
+mongoose.connect(url)
+
+const noteSchema = new mongoose.Schema({
+  content: String,
+  important: Boolean,
+})
+
+const Note = mongoose.model('Note', noteSchema)
+
+const note = new Note({
+  content: 'HTML is easy',
+  important: true,
+})
+
+note.save().then(result => {
+  console.log('note saved!')
+  mongoose.connection.close()
+})
