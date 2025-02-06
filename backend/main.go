125a068fc3d05dd91d3b735dd4804747078e2f4d
Ilkka Kivistö <ilkka.kivisto@gmail.com>
2025-02-06 21:45:02 +0200

    Move psql code to own package

--- backend/main.go
+++ backend/main.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/Slowers-team/Slowers-App/application"
 	"github.com/Slowers-team/Slowers-App/database"
+	psqldatabase "github.com/Slowers-team/Slowers-App/database/psql"
 	"github.com/Slowers-team/Slowers-App/handlers"
 )
 
@@ -22,9 +23,9 @@ func main() {
 		}
 	}
 
-	var sqldb *database.SQLDatabase
+	var sqldb *psqldatabase.SQLDatabase
 	if useSQL == "true" {
-		sqldb := database.NewSQLDatabase(SQLDatabaseURI)
+		sqldb := psqldatabase.NewSQLDatabase(SQLDatabaseURI)
 		if env == "test" {
 			if err := sqldb.Connect("SlowersTest"); err != nil {
 				log.Fatal(err)
