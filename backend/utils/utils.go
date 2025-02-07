245553ba0b655850dda693dc8527eab6386a0196
Hánno Länsman <halansman@gmail.com>
2025-02-07 14:19:37 +0200

    Add 'resizeImage' function

--- backend/utils/utils.go
+++ backend/utils/utils.go
@@ -5,6 +5,15 @@ import (
 	"log"
 	"regexp"
 
+	"fmt"
+	"image"
+	"image/gif"
+	"image/jpeg"
+	"image/png"
+	"io"
+
+	"golang.org/x/image/draw"
+
 	"golang.org/x/crypto/bcrypt"
 
 	"github.com/Slowers-team/Slowers-App/database"
@@ -43,4 +52,38 @@ func ToJSON(val any) []byte {
 		log.Fatal(err)
 	}
 	return asJSON
+}
+
+func resizeImage(input io.Reader, output io.Writer, format string, newWidth, newHeight int) error {
+	var src image.Image
+	var err error
+	switch format {
+	case "png":
+		src, err = png.Decode(input)
+	case "jpg", "jpeg":
+		src, err = jpeg.Decode(input)
+	case "gif":
+		src, err = gif.Decode(input)
+	default:
+		return fmt.Errorf("unsupported file type")
+	}
+	if err != nil {
+		return fmt.Errorf("failed to decode image: %w", err)
+	}
+	dst := image.NewRGBA(image.Rect(0, 0, newWidth, newHeight))
+	draw.NearestNeighbor.Scale(dst, dst.Rect, src, src.Bounds(), draw.Over, nil)
+	switch format {
+	case "png":
+		err = png.Encode(output, dst)
+	case "jpg", "jpeg":
+		err = jpeg.Encode(output, dst, nil)
+	case "gif":
+		err = gif.Encode(output, dst, nil)
+	default:
+		return fmt.Errorf("unsupported output file type")
+	}
+	if err != nil {
+		return fmt.Errorf("failed to encode image: %w", err)
+	}
+	return nil
 }
