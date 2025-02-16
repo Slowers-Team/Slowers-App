394579bf2c707cbdfdd8e2b471a9bfc7b6c52c25
Sirkkeli <siiri.kalliomaki@helsinki.fi>
2025-02-16 21:05:51 +0200

    Snail logo added

--- frontend/src/components/NavigationBar.jsx
+++ frontend/src/components/NavigationBar.jsx
@@ -1,10 +1,11 @@
 import "../App.css";
 import { useTranslation } from "react-i18next";
-import { Navbar, Nav, NavDropdown, Button, Offcanvas } from "react-bootstrap";
+import { Navbar, Nav, NavDropdown, Button, Offcanvas, Container } from "react-bootstrap";
 import LangSelect from "./LangSelect";
 import { Link, useLoaderData, Outlet, useFetcher } from "react-router-dom";
 import { useState } from "react";
 import { Authenticator } from "../Authenticator";
+import Snaillogo from "../images/etana3.svg"
 
 export const NavigationBar = () => {
   const { t, i18n } = useTranslation();
@@ -25,7 +26,7 @@ export const NavigationBar = () => {
           <span className="navbar-toggler-icon"></span>
         </Button>
         <Navbar.Brand as={Link} to="/">
-          <h1>Slowers</h1>
+          <h1><img width="50" src={Snaillogo} alt="snail"/>Slowers<img width="50" src={Snaillogo} alt="snail"/></h1>
         </Navbar.Brand>
         <Nav className="ms-auto mx-2">
           {isLoggedIn && (
