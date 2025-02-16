a421417c7088beae430161cfdae21e64e1477a80
Ellu <alikoskieveliina@gmail.com>
2025-02-16 20:09:19 +0200

    e2e tests for page visibility

--- frontend/cypress/e2e/logged_in.cy.js
+++ frontend/cypress/e2e/logged_in.cy.js
@@ -18,7 +18,46 @@ describe('Slowers ', function() {
 			cy.contains('Switch to Retailer').click()
 			cy.get('#roleValue').contains('Retailer')
 		})
+		describe('page visibility', function() {
+			beforeEach(function() {
+				cy.visit('/user')
+				cy.contains('test@email.com')
+			})
 
+			it('retailer user can not access grower page', function() {
+				cy.contains('Switch to Retailer').click()
+				cy.visit('grower')
+				cy.contains('Welcome to Slowers App')
+			})
+			it('grower user can not access retailer page', function() {
+				cy.contains('Grower')
+				cy.visit('retailer')
+				cy.contains('Welcome to Slowers App')
+			})
+			it('grower user can not access business owner page', function() {
+				cy.contains('Grower')
+				cy.visit('business_owner')
+				cy.contains('Welcome to Slowers App')
+			})
+			it('retailer can see authorized pages in side menu', function() {
+				cy.contains('Switch to Retailer').click()
+				cy.visit('home')
+				cy.get('#offcanvasButton').click()
+				cy.contains('Home')
+				cy.contains('Marketplace')
+				cy.contains('Retailer')
+				cy.contains('Terms')
+			})			
+			it('grower can see authorized pages in side menu', function() {
+				cy.contains('Grower')
+				cy.visit('home')
+				cy.get('#offcanvasButton').click()
+				cy.contains('Home')
+				cy.contains('Marketplace')
+				cy.contains('Grower page')
+				cy.contains('Terms')
+			})
+		})
 		describe('when a site has been added', function() {
 			beforeEach(function() {
 				cy.visit('/grower')
