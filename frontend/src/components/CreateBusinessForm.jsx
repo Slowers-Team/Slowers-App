679b0ed55d3f3c303e3d913ad38a03c44a74cf29
Lahja Lintumäki <lahja.lintumaki@gmail.com>
2025-02-18 12:21:32 +0200

    add form for creating business on user page

---
+++ frontend/src/components/CreateBusinessForm.jsx
@@ -0,0 +1,55 @@
+import { useState } from "react";
+import userService from "../services/users";
+import { useTranslation } from "react-i18next";
+
+const CreateBusinessForm = () => {
+	const [businessName, setBusinessName] = useState('')
+	const [type, setType] = useState ('')
+	const { t, i18n } = useTranslation()
+	console.log(type)
+
+	const handleCreateBusiness = (event) => {
+		event.preventDefault()
+		console.log('handleCreateBusiness reached')
+	}
+
+  return (
+    <div>
+      <form onSubmit={handleCreateBusiness}>
+				<div>
+					<table>
+						<tbody>
+							<tr>
+								<td>{t('businessform.fieldname.businessname')}</td>
+								<td>
+									<input 
+										className="form-control"
+										value={businessName}
+										placeholder={t('businessform.input.businessname')}
+										onChange={event => setBusinessName(event.target.value)}
+										style={{ width: "400px"}}
+									/>
+								</td>
+							</tr>
+							<tr>
+								<td>{t('businessform.fieldname.businesstype')}</td>
+								<td>
+									<label className='btn btn-outline-secondary' style={{ marginRight: "0.5rem" }} >
+										<input type="radio" className='btn-check' name="typeSelector" value="grower" onChange={event => setType(event.target.value)} />{t('button.grower')}
+									</label>
+									<label className='btn btn-outline-secondary' style={{ marginRight: "0.5rem" }} >
+									<input type="radio" className='btn-check' name="typeSelector" value="retailer" onChange={event => setType(event.target.value)} />{t('button.retailer')}
+									</label>
+								</td>
+							</tr>
+						</tbody>
+					</table>
+					<br/>
+					<button type="submit" className='custom-button'>{t('button.createbusiness')}</button>
+				</div>
+			</form>
+    </div>
+  )
+}
+
+export default CreateBusinessForm
