5339b8f6c405392bdb4fbcc5fd030c553412f30a
Ilkka Kivistö <ilkka.kivisto@gmail.com>
2025-03-04 15:20:32 +0200

    Differentiate production and development containers
    Co-authored-by: kaltionkasvatti <halansman@gmail.com>

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -11,7 +11,7 @@ export default defineConfig({
         changeOrigin: true,
       },
     },
-    allowedHosts: ["slowers-frontend"],
+    allowedHosts: ["slowers-frontend-dev"],
   },
   test: {
     environment: "jsdom",
