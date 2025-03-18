c3f1a7b38bb63044036d6b20a13f745e66c3d1cb
Sampinen <siiri.kalliomaki@helsinki.fi>
2025-03-18 15:29:14 +0200

    Grower page layout

--- frontend/src/pages/BusinessPage.jsx
+++ frontend/src/pages/BusinessPage.jsx
@@ -8,9 +8,33 @@ const BusinessPage = () => {
 
   return (
     <Container>
-      <h2>{t('title.businesspage')}</h2>
-      <br/>
-      <p>Here comes the content of the business page</p>
+      <center>
+      <div className="business-name-container">
+      <h1>Business name</h1>
+      <h2>Business type: Grower</h2>
+      <h2>Owner: Owner name</h2>
+      </div>
+      <br></br>
+
+      <div className="members-container">
+      <h3>Members:</h3>
+      <ul>
+        <li>Matti</li>
+        <li>Teppo</li>
+        <li>Maija</li>
+      </ul> 
+      <button className='add-member-button'>+Add member</button>
+      </div>
+      <br></br>
+
+      <div className="about-business-container">
+        <h3>About business:</h3>
+        <br></br>
+        <textarea id="businessinfo" name="businessinfo" rows="4" cols="50">About business</textarea>
+        <br></br>
+        <button className='add-member-button'>Update</button>
+      </div>
+      </center>
     </Container>
   );
 };
