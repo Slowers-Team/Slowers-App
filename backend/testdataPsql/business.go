596c95580ac6a6e8b34f0edee1b3ac2c13b6620d
Ilkka Kivistö <ilkka.kivisto@gmail.com>
2025-03-18 16:01:14 +0200

    Add test for creating business

---
+++ backend/testdataPsql/business.go
@@ -0,0 +1,21 @@
+package testdataPsql
+
+import (
+	database "github.com/Slowers-team/Slowers-App/database/psql"
+)
+
+func GetBusinesses() []database.Business {
+	return []database.Business{
+		{
+			ID:          1,
+			Name:        "Test Business",
+			Type:        "growing",
+			PhoneNumber: "010234567",
+			Email:       "tester@test.fi",
+			PostAddress: "Imaginary road 1",
+			PostalCode:  98765,
+			City:        "Flowertown",
+			Notes:       "No notes",
+		},
+	}
+}
