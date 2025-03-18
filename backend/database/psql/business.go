ad691b7303403d73260002247e43d13ced8e9b34
kaltionkasvatti <halansman@gmail.com>
2025-03-18 11:42:07 +0200

    Added CreateBusiness function that adds to psql database

--- backend/database/psql/business.go
+++ backend/database/psql/business.go
@@ -1,5 +1,7 @@
 package database
 
+import "context"
+
 type Business struct {
 	ID                  int
 	CreatedAt           string
@@ -12,4 +14,39 @@ type Business struct {
 	PostalCode          int
 	City                string
 	Notes               string
+}
+
+func (pDb SQLDatabase) CreateBusiness(ctx context.Context, newBusiness Business) (*Business, error) {
+
+	query := `
+	INSERT INTO Businesses (
+							business_name,
+							business_type,
+							business_phone_number,
+							business_email,
+							business_address,
+							postal_code,
+							city,
+							notes)
+	VALUES ($1, $2, $3, $4, $5, $6, $7, $8)
+	REURNING id`
+
+	err := pDb.pool.QueryRow(
+		ctx,
+		query,
+		newBusiness.BusinessName,
+		newBusiness.BusinessType,
+		newBusiness.BusinessPhoneNumber,
+		newBusiness.BusinessEmail,
+		newBusiness.BusinessAddress,
+		newBusiness.PostalCode,
+		newBusiness.City,
+		newBusiness.Notes,
+	).Scan(&newBusiness.ID)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return &newBusiness, nil
 }
