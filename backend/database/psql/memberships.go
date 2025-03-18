c132a33fa873b9d29e9eefa71d342c3552b9818b
kaltionkasvatti <halansman@gmail.com>
2025-03-18 14:05:41 +0200

    Added context to membership adding

--- backend/database/psql/memberships.go
+++ backend/database/psql/memberships.go
@@ -1,11 +1,14 @@
 package database
 
+import "context"
+
 type Membership struct {
 	UserID      int
 	BusinessID  int
 	Designation string
 }
 
-func (Db SQLDatabase) AddMembership(user_email string, business_email string, designation string) error {
+func (Db SQLDatabase) AddMembership(ctx *context.Context, user_email string, business_email string, designation string) error {
+
 	return nil
 }
