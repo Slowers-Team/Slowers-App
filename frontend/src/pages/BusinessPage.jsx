c6c0eaeb8211ee5ed4577b2a8d8589777fc88915
Sampinen <siiri.kalliomaki@helsinki.fi>
2025-03-21 14:08:12 +0200

    Business page has translations

--- frontend/src/pages/BusinessPage.jsx
+++ frontend/src/pages/BusinessPage.jsx
@@ -11,17 +11,19 @@ const BusinessPage = () => {
       <center>
       <div className="business-name-container">
       <h2>Business name</h2>
-      <h5>Business type: Grower</h5>
-      <h5>Phone number: +358 45 2789654</h5>
-      <h5>Email: business@email.com</h5>
-      <h5>Postal code: 00000</h5>
-      <h5>City: Kerava</h5> 
+      <h5>{t('businessform.fieldname.businesstype')}: Grower</h5>
+      <h5>{t('businessform.fieldname.phonenumber')}: +358 45 2789654</h5>
+      <h5>{t('businessform.fieldname.email')}: business@email.com</h5>
+      <h5>{t('businessform.fieldname.postalcode')}: 00000</h5>
+      <h5>{t('businessform.fieldname.city')}: Kerava</h5> 
+      <h5>{t('businessform.fieldname.address')}:Mustikkatie 8 A3</h5>
+      <h5>business id code: 23998</h5>
       <h5>Created: 23.5.2021</h5>
 
       <br></br>
       <br></br>
 
-      <h4>About business:</h4>
+      <h4>{t('businessform.fieldname.additionalinfo')}:</h4>
         <br></br>
         <div class="about-business-container">About business</div>
         <br></br>
