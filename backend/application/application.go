6a1861c56567793f3f9c3e8fbea51543ee489ec5
Ellu <alikoskieveliina@gmail.com>
2025-03-25 14:19:10 +0200

    Fixed unittest for GetUser in TestFetchingUser

--- backend/application/application.go
+++ backend/application/application.go
@@ -63,7 +63,12 @@ func SetupAndSetAuthTo(isAuthOn bool, useSQL bool) *fiber.App {
 	api.Delete("/sites/:id", handlers.DeleteSite)
 	api.Get("/sites/:id/flowers", handlers.GetSiteFlowers)
 
-	api.Get("/user", handlersPsql.GetUser)
+	if useSQL {
+		api.Get("/user", handlersPsql.GetUser)
+	} else {
+		api.Get("/user", handlers.GetUser)
+	}
+
 	api.Post("/user/role", handlers.SetRole)
 
 	api.Post("/business", handlersPsql.CreateBusiness)
