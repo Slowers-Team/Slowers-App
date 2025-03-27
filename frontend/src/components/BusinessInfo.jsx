f50f8a3856ac0f65c0fa8bbbf0cddc3c98fefc94
Lahja Lintumäki <lahja.lintumaki@gmail.com>
2025-03-27 15:51:17 +0200

    update to displaying business info

--- frontend/src/components/BusinessInfo.jsx
+++ frontend/src/components/BusinessInfo.jsx
@@ -1,37 +1,34 @@
-import { useTranslation } from "react-i18next";
-import { Container } from 'react-bootstrap'
+import { useTranslation } from "react-i18next"
 
 
 const BusinessInfo = ({ business }) => {
-  const { t, i18n } = useTranslation();
-
+  const { t, i18n } = useTranslation()
 
   return (
-    <Container>
-      <div className="business-name-container">
-      <h2>{ business.BusinessName }</h2>
-      <br></br>
-      <h5>{t('businessform.fieldname.businesstype')}: { business.Type }</h5>
-      <h5>{t('businessform.fieldname.phonenumber')}: { business.PhoneNumber }</h5>
-      <h5>{t('businessform.fieldname.email')}: { business.Email }</h5>
-      <h5>{t('businessform.fieldname.postalcode')}: { business.PostalCode }</h5>
-      <h5>{t('businessform.fieldname.city')}: { business.City }</h5> 
-      <h5>{t('businessform.fieldname.address')}: { business.Address }</h5>
-      <h5>{t('businessform.fieldname.businessidcode')}: { business.BusinessIdCode }</h5>
-      <h5>{t('businessform.fieldname.created')}: { business.CreatedAt }</h5>
-
-      <br></br>
-      <br></br>
-
-      <h4>{t('businessform.fieldname.additionalinfo')}:</h4>
-        <br></br>
-        <div class="about-business-container">{ business.AdditionalInfo }</div>
-        <br></br>
+    <div className="container">
+      <div className="row justify-content-center">
+        <div className="col-12 col-md-12 col-lg-12 col-xl-8">
+          <div className="card" style={{ borderRadius: "1rem" }}>
+            <div className="card-body p-5">
+              <h2>{ business.BusinessName }</h2>
+              <br/>
+              <h5>{t('businessform.fieldname.businesstype')}: { business.Type }</h5>
+              <h5>{t('businessform.fieldname.phonenumber')}: { business.PhoneNumber }</h5>
+              <h5>{t('businessform.fieldname.email')}: { business.Email }</h5>
+              <h5>{t('businessform.fieldname.postalcode')}: { business.PostalCode }</h5>
+              <h5>{t('businessform.fieldname.city')}: { business.City }</h5>
+              <h5>{t('businessform.fieldname.address')}: { business.Address }</h5>
+              <h5>{t('businessform.fieldname.businessidcode')}: { business.BusinessIdCode }</h5>
+              <h5>{t('businessform.fieldname.created')}: { business.CreatedAt }</h5>
+              <br/>
+              <h4>{t('businessform.fieldname.additionalinfo')}:</h4>
+              <h5>{ business.AdditionalInfo }</h5>
+            </div>
+          </div>
+        </div>
       </div>
-      
-      <br></br>
-    </Container>
-  );
-};
+    </div>
+  )
+}
 
-export default BusinessInfo;
+export default BusinessInfo
