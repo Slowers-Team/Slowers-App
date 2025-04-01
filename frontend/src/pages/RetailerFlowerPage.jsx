3fb90a5e5880c36ec31dc16ff7b8c8e9763aa60c
Lahja Lintumäki <lahja.lintumaki@gmail.com>
2025-04-01 13:50:43 +0300

    Containers to homepage and marketplace

--- frontend/src/pages/RetailerFlowerPage.jsx
+++ frontend/src/pages/RetailerFlowerPage.jsx
@@ -12,10 +12,18 @@ const RetailerFlowerPage = () => {
   }, [])
 
   return (
-    <Container>
-      <h2>{t('title.flowers')}</h2>
-      {flowers && <RetailerFlowerList flowers={flowers} />}
-    </Container>
+    <div className="m-3">
+      <div className="row justify-content-center">
+        <div className="col-12 col-md-12 col-lg-12 col-xl-12">
+          <div className="card" style={{ borderRadius: "1rem" }}>
+            <div className="card-body p-5">
+              <h2>{t('title.flowers')}</h2>
+              {flowers && <RetailerFlowerList flowers={flowers} />}
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
