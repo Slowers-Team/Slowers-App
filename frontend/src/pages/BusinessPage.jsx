eedf6f0933c8f6e661670197aaa5cfd11038d6ce
Lahja Lintumäki <lahja.lintumaki@gmail.com>
2025-04-01 11:30:29 +0300

    margin fixed in business page and small refactoring

--- frontend/src/pages/BusinessPage.jsx
+++ frontend/src/pages/BusinessPage.jsx
@@ -16,9 +16,17 @@ const BusinessPage = () => {
   }, []);
 
   return (
-    <Container className="m-3">
-      <BusinessInfo business={business} />
-    </Container>
+    <div className="m-3">
+      <div className="row justify-content-center">
+        <div className="col-12 col-md-12 col-lg-12 col-xl-8">
+          <div className="card" style={{ borderRadius: "1rem" }}>
+            <div className="card-body p-5">
+              <BusinessInfo business={business} />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
