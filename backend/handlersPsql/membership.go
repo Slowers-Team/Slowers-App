8fab74129db73a54ccae19ead5464130adddd64b
Ellu <alikoskieveliina@gmail.com>
2025-04-04 15:25:06 +0300

    Functions for adding membership in front and backend

--- backend/handlersPsql/membership.go
+++ backend/handlersPsql/membership.go
@@ -10,6 +10,9 @@ import (
 )
 
 func AddMembership(c *fiber.Ctx, membership *database.Membership) error {
+	// onko tää turha??
+	// koska aina kutsutaan jostain toisesta funktiosta, jossa
+	// annetaan membership, eli ei pitäis joutuu BodyParseroimaan
 	if membership == nil {
 		membership = new(database.Membership)
 
@@ -17,10 +20,27 @@ func AddMembership(c *fiber.Ctx, membership *database.Membership) error {
 			return c.Status(400).SendString(err.Error())
 		}
 	}
+	// tähän asti ???
 
 	_, err := db.AddMembership(c.Context(), *membership)
 	if err != nil {
 		fmt.Println("Jäsenyyden lisääminen epäonnistui")
+		return c.Status(500).SendString(err.Error())
+	}
+
+	return c.SendStatus(204)
+}
+
+func AddMembershipHelper(c *fiber.Ctx) error {
+	membership := new(database.Membership)
+
+	if err := c.BodyParser(membership); err != nil {
+		return c.Status(400).SendString(err.Error())
+	}
+	fmt.Println(membership)
+
+	if err := AddMembership(c, membership); err != nil {
+		fmt.Println("Jäsenen lisäys epäonnistui")
 		return c.Status(500).SendString(err.Error())
 	}
 
