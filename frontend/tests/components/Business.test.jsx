d2dfa786f4f273837e625a433325ff8323549064
Sampinen <siiri.kalliomaki@helsinki.fi>
2025-04-11 15:40:01 +0300

    First test

---
+++ frontend/tests/components/Business.test.jsx
@@ -0,0 +1,40 @@
+import userEvent from '@testing-library/user-event'
+import { render, screen } from '@testing-library/react'
+import CreateBusinessForm from '../../src/components/CreateBusinessForm'
+
+
+
+test('sends form if fields are filled correctly', async () =>{
+    render(<CreateBusinessForm CreateBusinessForm = {CreateBusinessForm}/>)
+
+    const user = userEvent.setup()
+
+    const Businessname = screen.getByPlaceholderText('Enter the name of the business')
+    const BusinessID = screen.getByPlaceholderText('Enter business ID')
+    const TypeOfTheBusiness = screen.getByText('Retailer')
+    const BusinessEmail = screen.getAllByPlaceholderText('Enter email')
+    const PhoneNumber = screen.getByPlaceholderText('Enter phone number')
+    const Address = screen.getByPlaceholderText('Enter address')
+    const PostalCode = screen.getByPlaceholderText('Enter postal code')
+    const City = screen.getByPlaceholderText('Enter city/municipality')
+    const AdditionalInformation = screen.getByPlaceholderText('Enter additional information')
+
+    const CreateBusiness = screen.getByText('Create business')
+
+
+
+    await user.type(Businessname, 'Flower Business') 
+    await user.type(BusinessID, '1234567-8')
+    await user.click(TypeOfTheBusiness)
+    await user.type(PhoneNumber, '0501234567')
+    await user.type(BusinessEmail, 'flower@business.com')
+    await user.type(Address, 'Kukkatie 9A99')
+    await user.type(PostalCode, '99999')
+    await user.type(City, 'Korvatunturi')
+    await user.type(AdditionalInformation, 'We sell flowers')
+
+    await user.click(CreateBusiness)
+
+    // screen.getByText('Created:')
+})
+
