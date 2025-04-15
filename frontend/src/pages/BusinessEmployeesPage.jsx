45df299878d2c025bd98bb3c60784ec3ea9b9260
Ellu <alikoskieveliina@gmail.com>
2025-04-15 10:21:04 +0300

    AddEmployeeForm is visible only to owners

--- frontend/src/pages/BusinessEmployeesPage.jsx
+++ frontend/src/pages/BusinessEmployeesPage.jsx
@@ -4,11 +4,13 @@ import businessService from "../services/business"
 import EmployeesList from "../components/EmployeesList"
 import { useTranslation } from 'react-i18next'
 import AddEmployeeForm from "../components/AddEmployeeForm"
+import { Authenticator } from "../Authenticator"
 
 
 const BusinessEmployeesPage = () => {
   const [employees, setEmployees] = useState([])
   const { t, i18n } = useTranslation()
+  const designation = Authenticator.designation
   //const [errorMessage, setErrorMessage] = useState("")
   const employeeGetter = async () => {
     const business = await businessService.get();
@@ -28,7 +30,9 @@ const BusinessEmployeesPage = () => {
         <div className="col-12 col-md-12 col-lg-12 col-xl-8">
           <div className="card" style={{ borderRadius: "1rem" }}>
             <div className="card-body p-5">
-              <AddEmployeeForm onEmployeeAdded={employeeGetter}/>
+              {designation === 'owner' && (
+                <AddEmployeeForm onEmployeeAdded={employeeGetter}/>
+              )}
               <br/>
               <h2>{ t("menu.employees") }</h2>
               <EmployeesList employees={employees} />              
