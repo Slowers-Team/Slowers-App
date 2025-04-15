77b4991a1c053cb0b6a77dddf4aefccd16c308bd
Sampinen <siiri.kalliomaki@helsinki.fi>
2025-04-15 12:01:39 +0300

    Added more frontend tests

--- frontend/tests/components/CreateBusinessForm.test.jsx
+++ frontend/tests/components/CreateBusinessForm.test.jsx
@@ -44,6 +44,19 @@ test('does not send an empty form', async () =>{
     const user = userEvent.setup()
     render(<CreateBusinessForm createNewBusiness = {createNewBusiness}/>)
 
+    const CreateBusiness = screen.getByText('Create business')
+
+    await user.click(CreateBusiness)
+    expect(createNewBusiness.mock.calls).toHaveLength(0)
+})
+
+
+test('does not send a form with incorrect input', async () =>{
+
+    const createNewBusiness = vi.fn()
+    const user = userEvent.setup()
+    render(<CreateBusinessForm createNewBusiness = {createNewBusiness}/>)
+
     const Businessname = screen.getByPlaceholderText('Enter the name of the business')
     const BusinessID = screen.getByPlaceholderText('Enter business ID')
     const TypeOfTheBusiness = screen.getByText('Retailer')
@@ -56,15 +69,93 @@ test('does not send an empty form', async () =>{
 
     const CreateBusiness = screen.getByText('Create business')
 
-    await user.type(Businessname, '') 
-    await user.type(BusinessID, '')
+    await user.type(Businessname, 'ABC') 
+    await user.type(BusinessID, 'ABC')
     await user.click(TypeOfTheBusiness)
-    await user.type(PhoneNumber, '')
-    await user.type(BusinessEmail, '')
-    await user.type(Address, '')
-    await user.type(PostalCode, '')
-    await user.type(City, '')
-    await user.type(AdditionalInformation, '')
+    await user.type(PhoneNumber, 'ABC')
+    await user.type(BusinessEmail, 'ABC')
+    await user.type(Address, 'ABC')
+    await user.type(PostalCode, 'ABC')
+    await user.type(City, 'ABC')
+    await user.type(AdditionalInformation, 'ABC')
+
     await user.click(CreateBusiness)
+
     expect(createNewBusiness.mock.calls).toHaveLength(0)
+})
+
+test('Updates input after typing', async () =>{
+
+    const createNewBusiness = vi.fn()
+    const user = userEvent.setup()
+    render(<CreateBusinessForm createNewBusiness = {createNewBusiness}/>)
+
+    const Businessname = screen.getByPlaceholderText('Enter the name of the business')
+    const BusinessID = screen.getByPlaceholderText('Enter business ID')
+    const TypeOfTheBusiness = screen.getByText('Retailer')
+    const BusinessEmail = screen.getByPlaceholderText('Enter email')
+    const PhoneNumber = screen.getByPlaceholderText('Enter phone number')
+    const Address = screen.getByPlaceholderText('Enter address')
+    const PostalCode = screen.getByPlaceholderText('Enter postal code')
+    const City = screen.getByPlaceholderText('Enter city/municipality')
+    const AdditionalInformation = screen.getByPlaceholderText('Enter additional information')
+
+
+    await user.type(Businessname, 'ABC') 
+    await user.type(BusinessID, 'ABC')
+    await user.click(TypeOfTheBusiness)
+    await user.type(PhoneNumber, 'ABC')
+    await user.type(BusinessEmail, 'ABC')
+    await user.type(Address, 'ABC')
+    await user.type(PostalCode, 'ABC')
+    await user.type(City, 'ABC')
+    await user.type(AdditionalInformation, 'ABC')
+
+    expect(Businessname.value).toBe('ABC')
+    expect(BusinessID.value).toBe('ABC')
+    expect(PhoneNumber.value).toBe('ABC')
+    expect(BusinessEmail.value).toBe('ABC')
+    expect(Address.value).toBe('ABC')
+    expect(PostalCode.value).toBe('ABC')
+    expect(City.value).toBe('ABC')
+    expect(AdditionalInformation.value).toBe('ABC')
+
+})
+
+test('Does not clear fields when sending a form with incorrect input', async () =>{
+    const createNewBusiness = vi.fn()
+    const user = userEvent.setup()
+    render(<CreateBusinessForm createNewBusiness = {createNewBusiness}/>)
+
+    const Businessname = screen.getByPlaceholderText('Enter the name of the business')
+    const BusinessID = screen.getByPlaceholderText('Enter business ID')
+    const TypeOfTheBusiness = screen.getByText('Retailer')
+    const BusinessEmail = screen.getByPlaceholderText('Enter email')
+    const PhoneNumber = screen.getByPlaceholderText('Enter phone number')
+    const Address = screen.getByPlaceholderText('Enter address')
+    const PostalCode = screen.getByPlaceholderText('Enter postal code')
+    const City = screen.getByPlaceholderText('Enter city/municipality')
+    const AdditionalInformation = screen.getByPlaceholderText('Enter additional information')
+
+    const CreateBusiness = screen.getByText('Create business')
+
+    await user.type(Businessname, 'ABC') 
+    await user.type(BusinessID, 'ABC')
+    await user.click(TypeOfTheBusiness)
+    await user.type(PhoneNumber, 'ABC')
+    await user.type(BusinessEmail, 'ABC')
+    await user.type(Address, 'ABC')
+    await user.type(PostalCode, 'ABC')
+    await user.type(City, 'ABC')
+    await user.type(AdditionalInformation, 'ABC')
+    await user.click(CreateBusiness)
+
+    expect(Businessname.value).toBe('ABC')
+    expect(BusinessID.value).toBe('ABC')
+    expect(PhoneNumber.value).toBe('ABC')
+    expect(BusinessEmail.value).toBe('ABC')
+    expect(Address.value).toBe('ABC')
+    expect(PostalCode.value).toBe('ABC')
+    expect(City.value).toBe('ABC')
+    expect(AdditionalInformation.value).toBe('ABC')
 })
